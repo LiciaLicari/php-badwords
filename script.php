8055ca6a4e40b7729ef12047e0f9706e799801f5
“LiciaLicari” <licianbsplicari@gmail.com>
2023-10-09 16:07:20 +0200

    fix

--- script.php
+++ script.php
@@ -2,7 +2,7 @@
 
 $text = $_POST['text'];
 $badword = $_POST['badword'];
-
+$final_text = str_replace($badword, '***', $text);
 ?>
 
 <!DOCTYPE html>
@@ -16,7 +16,7 @@ $badword = $_POST['badword'];
 <h3>Typed word:</h3> <span><p><?php echo $text; ?></p></span>
     <h5>Text is <?php echo strlen($text) ?> letters</h5>
 
-    <h3>Censored word is:</h3><span><?php echo str_replace($badword,'***', $text ); ?></p></span>
+    <h3>Censored word is:</h3><span><?php echo $final_text ?></p></span>
 
 </body>
 </html>
